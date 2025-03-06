0c7305d933d431787207e50bd227194f05879c58
rintarotajima <rintarotajima@gmail.com>
2025-03-06 18:52:17 +0900

    ジェネリックを使用して再利用可能なクラスを作成

---
+++ GenericsAndObjectsAndExtensions/generics.kt
@@ -0,0 +1,29 @@
+// class FillinTheBlankQuestion(
+//     val questionText: String,
+//     val answer: String,
+//     val difficulty: String
+// )
+
+// class TrueOrFalseQuestion(
+//     val questionText: String,
+//     val answer: Boolean,
+//     val difficulty: String
+// )
+
+// class NumericQuestion(
+//     val questionText: String,
+//     val answer: Int,
+//     val difficulty: String
+// )
+
+class Question<T>(
+    val questionText: String,
+    val answer: T,
+    val difficulty: String
+)
+
+fun main() {
+    val question1 = Question<String>("Quoth the raven ___", "nevermore", "medium")
+    val question2 = Question<Boolean>("The sky is greeen. True or False", false, "easy")
+    val question3 = Question<Int>("How many days are there between full moons?", 28, "hard")
+}

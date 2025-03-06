1d13ccbe4d1438a2cfc6fc882ed811ed8f205b58
rintarotajima <rintarotajima@gmail.com>
2025-03-06 19:01:46 +0900

    列挙型クラスを使用

--- GenericsAndObjectsAndExtensions/generics.kt
+++ GenericsAndObjectsAndExtensions/generics.kt
@@ -19,11 +19,15 @@
 class Question<T>(
     val questionText: String,
     val answer: T,
-    val difficulty: String
+    val difficulty: Difficulty
 )
 
+enum class Difficulty {
+    EASY, MEDIUM, HARD
+}
+
 fun main() {
-    val question1 = Question<String>("Quoth the raven ___", "nevermore", "medium")
-    val question2 = Question<Boolean>("The sky is greeen. True or False", false, "easy")
-    val question3 = Question<Int>("How many days are there between full moons?", 28, "hard")
+    val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
+    val question2 = Question<Boolean>("The sky is greeen. True or False", false, Difficulty.EASY)
+    val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
 }

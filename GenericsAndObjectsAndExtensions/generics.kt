7a74dc1b1323cdb0fe42a350dafcb73b4ad1e21b
rintarotajima <rintarotajima@gmail.com>
2025-03-07 17:18:23 +0900

    インターフェースで拡張関数を書き換える

--- GenericsAndObjectsAndExtensions/generics.kt
+++ GenericsAndObjectsAndExtensions/generics.kt
@@ -26,7 +26,12 @@ enum class Difficulty {
     EASY, MEDIUM, HARD
 }
 
-class Quiz {
+interface ProgressPrintable {
+    val progressText: String
+    fun printProgressBar() 
+}
+
+class Quiz: ProgressPrintable {
     val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
     val question2 = Question<Boolean>("The sky is greeen. True or False", false, Difficulty.EASY)
     val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
@@ -36,20 +41,29 @@ class Quiz {
         var answered: Int = 3
     }
 
+    override val progressText: String
+        get() = "${answered} of ${total} answered"
+
+    override fun printProgressBar() {
+        repeat(Quiz.answered) { print("□")}
+        repeat(Quiz.total - Quiz.answered) { print("■")}
+        println()
+        println(progressText)
+    }
 }
 
-val Quiz.StudentProgress.progressText: String
-    get() = "${answered} of ${total} answered"
+// val Quiz.StudentProgress.progressText: String
+//     get() = "${answered} of ${total} answered"
 
-fun Quiz.StudentProgress.progressBar() {
-    repeat(Quiz.answered) { print("▓") }
-    repeat(Quiz.total - Quiz.answered) { print("▒") }
-    println()
-    println(Quiz.progressText)
-}
+// fun Quiz.StudentProgress.progressBar() {
+//     repeat(Quiz.answered) { print("▓") }
+//     repeat(Quiz.total - Quiz.answered) { print("▒") }
+//     println()
+//     println(Quiz.progressText)
+// }
 
 
 fun main() {
-    Quiz.progressBar()
+    Quiz().printProgressBar()
 }
 
